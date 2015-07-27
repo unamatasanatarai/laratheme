560a0d082fe04188ace14495629a540b98ddf8d4
Piotr <piotr.grabowski@cleapps.com>
2015-07-27 12:16:21 +0200

    automated

--- src/LaraTheme.php
+++ src/LaraTheme.php
@@ -1,21 +1,76 @@
 <?php
 
 class LaraTheme {
-    protected $theme = '';
-    protected $layout = 'default';
+    static protected $theme = '';
+    static protected $layout = 'default';
 
-    public function render($view, $vars = [])
-    {
-        $path = [];
-        if (!empty(self::$theme)){
-            $path[] = self::$theme;
-        }
-        $path[] = $view;
-
-        return view($path, $vars);
+    /**
+     * Set the name of the THEME views/themes/NAME
+     * @param [type] $name [description]
+     */
+    static public function setTheme($name){
+        self::$theme = trim($name);
     }
 
-    public function element($element, $vars = []){
-        return self::render('elements' . $element, $vars);
+    /**
+     * Set the name of the Layout views/theme/SETTHEME/layouts/LAYOUT.blade.php
+     * @param [type] $name [description]
+     */
+    static public function setLayout($name){
+        self::$layout = trim($name);
+    }
+
+    /**
+     * Validate Theme access path (dot-path)
+     * @return [type] [description]
+     */
+    static public function getThemePath(){
+        if (empty(self::$theme)){
+            return '';
+        }
+
+        return 'themes.' . self::$theme;
+    }
+
+    /**
+     * Get the path to the layout
+     * @return [type] [description]
+     */
+    static public function layout(){
+        $path = self::getThemePath();
+        if (empty($path)){
+            return self::$layout;
+        }
+        return $path . '.layouts.' . self::$layout;
+    }
+
+    /**
+     * Get the path to the element
+     * @param  [type] $view [description]
+     * @return [type]       [description]
+     */
+    static public function element($view){
+        $path = array_filter([
+            self::getThemePath(),
+            'elements',
+            $view
+        ]);
+        return implode('.', $path);
+    }
+
+    /**
+     * Render the view accordingly to the Theme/Layout setup
+     * @param  [type] $view [description]
+     * @param  array  $vars [description]
+     * @return [type]       [description]
+     */
+    static public function render($view, $vars = [])
+    {
+        $path = array_filter([
+            self::getThemePath(),
+            $view
+        ]);
+
+        return view(implode('.', $path), $vars);
     }
 }
